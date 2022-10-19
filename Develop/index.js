e0eee919f0bc430c6ddba738c97f285824c387e1
Morgan Kelly <mkelly5380@gmail.com>
2022-10-19 15:50:18 -0500

    added npm inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,40 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+// const util = require("util");
+const inquirer = require("inquirer");
+// const generateReadme = require("./utils/generateReadme")
+// const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your user name?',
+      name: 'username',
+    },
+    {
+      type: 'password',
+      message: 'What is your password?',
+      name: 'password',
+    },
+    {
+      type: 'password',
+      message: 'Re-enter password to confirm:',
+      name: 'confirm',
+    },
+  ])
+  .then((response) => {
+    console.log(response)
+  }
+  );
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// Function call to initialize app
-init();
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
